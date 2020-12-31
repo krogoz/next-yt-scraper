2dcb540cbd02dcdf2a3eeeb368ddb1cf5fa01c31
Kacper Rogoz <kacper@pop-os.localdomain>
2020-12-30 20:48:57 -0500

    change: getVideoFromUrl return from false to ''

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ const Home = () => {
   const getVideoIdFromUrl = url => {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
     var match = url.match(regExp);
-    return (match&&match[7].length==11)? match[7] : false
+    return (match&&match[7].length==11)? match[7] : ''
   }
 
   /**
